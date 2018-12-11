7c4a8ec207544584471b9003980f51e16b362e67
Pedro V. B. Jeronymo <pedrovbj@gmail.com>
2018-12-10 22:35:17 -0200

    License added as header in .py files and more documentation

--- preproc.py
+++ preproc.py
@@ -1,3 +1,35 @@
+# MIT License
+#
+# Copyright (c) 2018 Pedro Virgilio Basilio Jeronymo
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+'''
+SVHN Dataset Pre-processor
+
+- Pre-processes SVHN dataset to a more convinient format for my ML models.
+- Provides some utilities as flatten.
+- Run it as main to pre-process the data.
+
+More info on github.com/pedrovbj/SVHN-ML
+'''
+
 import numpy as np
 from scipy.io import loadmat
 from sklearn.utils import shuffle
@@ -5,26 +37,49 @@ from sklearn.utils import shuffle
 # Path to data directory
 path = '../SVHN/'
 
-def y2indicator(y):
+def y2indicator(y, n_classes=10):
+    '''
+    Transforms a categorical targets vector to a one-hot encoded vector
+
+    y: input vector
+    n_classes: number of classes
+
+    Example:
+        INPUT:  y = np.array([0, 1, 2]), n_classes=3
+        OUTPUT: y_ind = np.array([[1, 0, 0]
+                                  [0, 1, 0]
+                                  [0, 0, 1]])
+    '''
     N = len(y)
-    ind = np.zeros((N, 10))
+    ind = np.zeros((N, n_classes))
     for i in range(N):
         ind[i, y[i]] = 1
     return ind
 
 def flatten(X):
+    '''
+    Flatten all but the first dimension of a numpy array X
+    '''
     return X.reshape(X.shape[0], -1)
 
 def rearrange(X):
+    '''
+    Rearrange the input array X
+    from (x-coord, y-coord, features, sample number)
+    to (sample number, features, x-coord, y-coord)
+    '''
     N = X.shape[-1]
     out = np.zeros((N, 3, 32, 32), dtype=np.float32)
 
     for i in range(N):
         for j in range(3):
             out[i, j, :, :] = X[:, :, j, i]
-    return out/255
+    return out
 
 def load_data():
+    '''
+    Loads the previously pre-processed data
+    '''
     Xtrain = np.load(path+'Xtrain.npy')
     Ytrain = np.load(path+'Ytrain.npy')
     Ytrain_ind = np.load(path+'Ytrain_ind.npy')
